a21d5d1e90e489153a673fafbbb617611609830f
karim-sebih <karim.sebih@laplateforme.io>
2025-02-03 12:10:36 +0100

    job 1et2

--- jour1/job2/job2..php
+++ jour1/job2/job2..php
@@ -7,6 +7,7 @@ public int $nombre2;
 public function __construct($nombre1,$nombre2){
     $this->nombre1 = $nombre1;
     $this->nombre2 = $nombre2;
+$this ->nombre3=$nombre1 + $nombre2;
 }
 
 }
@@ -15,8 +16,7 @@ public function __construct($nombre1,$nombre2){
 $operation = new Operation ("1","2");
 
 
-echo"nombre 1 :" . $operation->nombre1;
-echo"nombre 2 :" . $operation->nombre2;
+echo"resultat:" . $operation->nombre3;
 
 
 
